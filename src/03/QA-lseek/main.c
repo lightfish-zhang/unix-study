1ad064dd046cf381093b81f5562cf20de0d6d2aa
lightfish <coolyeah@foxmail.com>
2017-06-14 09:54:42 +0800

    update chapter 3 answer

--- src/03/QA-lseek/main.c
+++ src/03/QA-lseek/main.c
@@ -5,6 +5,44 @@
 #include <fcntl.h>
 #include <unistd.h>
 
+int readLimit(int fd, int start, int size){
+    char readBuffer[size];
+    if(lseek(fd, start, SEEK_SET) < 0){
+        perror("error, lseek()");
+        return -1;
+    }
+
+    if(read(fd, readBuffer, size) < 0){
+        perror("error, read()");
+        return -1;
+    }
+
+    readBuffer[size] = '\0';
+    //printf("after lseek(fd, %d, SEEK_SET), read %d size: %s\n", start, size, readBuffer);
+    printf("after lseek(fd, %d, SEEK_SET), read %d size: ", start, size);
+
+    int i;
+    for(i=0; i<size; i++){
+        if(readBuffer[i])
+            putc(readBuffer[i], stdout);
+        else
+            fputs("\\0", stdout);
+    }
+    putc('\n', stdout);
+}
+
+int writeLimit(int fd, int start, char * buffer, size){
+    if(lseek(fd, start, SEEK_SET) < 0){
+        perror("error, lseek()");
+        return -1;
+    }
+    if(write(fd, buffer, size) < 0){
+        perror("error, write()");
+        return -1;
+    }
+    return 0;
+}
+
 int
 main()
 {
@@ -32,19 +70,40 @@ main()
         exit(1);
     }
 
-    printf("read: %s\n", readBuffer);
+    printf("after open(tmpFile, O_RDWR) read: %s\n", readBuffer);
 
-    if(lseek(fd, 0, SEEK_SET) < 0){
-        perror("error, lseek()");
+    readLimit(fd, 0, size);
+    readLimit(fd, 26, 10);
+    readLimit(fd, 26, 20);
+
+    writeLimit(fd, 1, "aaa", 3);
+    readLimit(fd, 0, size);
+    close(fd);
+
+
+    fd = open(tmpFile, O_APPEND|O_RDWR);
+    if(fd<0){
+        perror("error, open()");
         exit(1);
     }
+    if(write(fd, "ccc", 3) < 0){
+        perror("error, write()");
+    }
+    readLimit(fd, 0, size+3);
+    close(fd);
 
-    if(read(fd, readBuffer, size) < 0){
-        perror("error, read()");
+
+
+    fd = open(tmpFile, O_RDWR);
+    if(fd<0){
+        perror("error, open()");
         exit(1);
     }
-
-    printf("after lseek, read: %s\n", readBuffer);
+    writeLimit(fd, 1, "aaa", 3);
+    readLimit(fd, 0, size);
+    writeLimit(fd, 100, "bbb", 3);
+    readLimit(fd, 0, 103); // hole file, there are '\0' where no data
+    close(fd);
 
     exit(0);
 }
